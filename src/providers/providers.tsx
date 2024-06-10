16d5e228527338316081945342de7c8457231248
Хижа Кирилл <scerep529@gmail.com>
2024-06-10 18:26:30 +0600

    create yandex metrika

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { YandexMetricaProvider } from 'next-yandex-metrica'
 import { PropsWithChildren, useState } from 'react'
 
 import ModalProvider from './Modal.provider'
@@ -17,11 +18,20 @@ export function Providers({ children }: PropsWithChildren) {
 		})
 	)
 	return (
-		<QueryClientProvider client={client}>
-			<SmoothScrollProvider>
-				<ModalProvider>{children}</ModalProvider>
-			</SmoothScrollProvider>
-			{/* <ReactQueryDevtools initialIsOpen={false} /> */}
-		</QueryClientProvider>
+		<YandexMetricaProvider
+			tagID={97533344}
+			initParameters={{
+				clickmap: true,
+				trackLinks: true,
+				accurateTrackBounce: true
+			}}
+		>
+			<QueryClientProvider client={client}>
+				<SmoothScrollProvider>
+					<ModalProvider>{children}</ModalProvider>
+				</SmoothScrollProvider>
+				{/* <ReactQueryDevtools initialIsOpen={false} /> */}
+			</QueryClientProvider>
+		</YandexMetricaProvider>
 	)
 }
